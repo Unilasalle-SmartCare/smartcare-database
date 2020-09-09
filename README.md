d3762b5c03779a62da84977ef3bda40e0b3b7c61
Luiza Fortes <luizafortes@gmail.com>
2020-09-08 21:31:39 -0300

    DCC, DED e Script de criação das tabelas no banco.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # smartcare-database
- 
+
+## Diagrama de Classe Conceitual
+![DCC](https://github.com/Unilasalle-SmartCare/smartcare-database/diagramas/DCC.png)
---
+++ smartcare_db.sql
@@ -0,0 +1,37 @@
+
+-- -----------------------------------------------------
+-- Table `dbo`.`Dispositivo`
+-- Drop Table Dispositivo
+-- -----------------------------------------------------
+CREATE TABLE Dispositivo (
+  IdDispositivo INT NOT NULL,
+  IdTipo INT NOT NULL,
+  Nome NVARCHAR(40) NOT NULL,
+  Descricao NVARCHAR(200) NULL,
+  Eixo_X FLOAT NULL,
+  Eixo_Y FLOAT NULL,
+  PRIMARY KEY (IdDispositivo),
+  FOREIGN KEY (IdTipo) REFERENCES Tipo(IdTipo))
+go
+-- -----------------------------------------------------
+-- `Tipo`
+-- Drop Table Tipo
+-- -----------------------------------------------------
+CREATE TABLE Tipo (
+  IdTipo INT NOT NULL,
+  Nome NVARCHAR(40) NOT NULL,
+  PRIMARY KEY (IdTipo))
+go
+-- -----------------------------------------------------
+-- `Medicao`
+-- Drop Table Medicao
+-- -----------------------------------------------------
+CREATE TABLE Medicao (
+  IdMedicao INT NOT NULL,
+  IdDispositivo INT NULL,
+  DataHora DATETIME NULL,
+  Valor DECIMAL(5,2) NULL,
+  Unidade CHAR(4) NULL,
+  PRIMARY KEY (IdMedicao),
+  FOREIGN KEY (IdDispositivo) REFERENCES Dispositivo(IdDispositivo))
+  go
